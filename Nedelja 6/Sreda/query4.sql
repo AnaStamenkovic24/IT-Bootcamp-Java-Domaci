d0334481531f63e0378555bcda3536d38d8d9f13
StrahinjaTBC <87874998+StrahinjaTBC@users.noreply.github.com>
2021-09-07 20:46:12 +0200

    Create query4.sql

---
+++ Nedelja 6/Sreda/query4.sql
@@ -0,0 +1,17 @@
+INSERT INTO Ucenici
+VALUES (1, 'Strahinja', 'Trninic', 24, 7.90);
+
+INSERT INTO Ucenici
+VALUES (2, 'Cvijan', 'Peranovic', 23, 1.0);
+
+INSERT INTO Ucenici
+VALUES (3, 'Milena', 'Bajic', 23, 9.0);
+
+INSERT INTO Ucenici
+VALUES (4, 'To', 'Oru', 10, 4.25);
+
+INSERT INTO Ucenici
+VALUES (5, 'Harry', 'Potter', 15, 3.0);
+
+INSERT INTO Ucenici
+VALUES (6, 'Aleksandar', 'Jevtic', 12, 5.0);
