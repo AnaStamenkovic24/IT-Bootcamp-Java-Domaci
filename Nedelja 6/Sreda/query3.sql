127a27ebd35e734833c77b9ce49e8fa866cc7a9c
StrahinjaTBC <87874998+StrahinjaTBC@users.noreply.github.com>
2021-09-07 20:45:51 +0200

    Create query3.sql

---
+++ Nedelja 6/Sreda/query3.sql
@@ -0,0 +1,8 @@
+INSERT INTO Profesori
+VALUES (1, 'Nebojsa', 'Ikodinovic', 48, 'Logika');
+
+INSERT INTO Profesori
+VALUES (2, 'Filip', 'Maric', 44, 'Racunarstvo');
+
+INSERT INTO Profesori
+VALUES (3, 'Srdjan', 'Vukmirovic', 50, 'Geometrija');
